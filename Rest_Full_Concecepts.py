619104aa280883d4b985a85300a282a291ce9957
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-02 14:53:10 +0300

    Create Rest_Full_Concecepts.py

---
+++ Rest_Full_Concecepts.py
@@ -0,0 +1,145 @@
+Restful Api can be broken down into three concepts
+
+Resources
+Resource represenation
+Http Methods
+
+
+
+
+
+In a RESTful API, resources are the fundamental concept around which the API is designed.
+A resource is an object or entity that is exposed by the API and can be accessed by a unique URL. 
+Examples of resources might include users, products, orders, or blog posts.
+
+Each resource is represented by a resource representation, which is a collection of data that describes the state of the resource at a given point in time.
+Resource representations are typically serialized into a format such as JSON or XML so that they can be transmitted over the network.
+
+HTTP methods are used to interact with resources in a RESTful API. The four most common HTTP methods used in RESTful APIs are:
+
+
+GET: Retrieves a representation of a resource. This method is used to read data from the server.
+
+POST: Creates a new resource. This method is used to submit data to the server to create a new resource.
+
+PUT: Updates an existing resource. This method is used to update the state of an existing resource on the server.
+
+DELETE: Deletes a resource. This method is used to delete a resource from the server.
+
+
+
+
+In addition to these four methods, there are several other HTTP methods that can be used in a RESTful API,
+including HEAD, OPTIONS, PATCH, and others. Each of these methods has a specific purpose and can be used to perform different actions on resources.
+
+Overall, the use of resources, resource representations, and HTTP methods is a key part of the design of a RESTful API.
+By following the principles of REST, developers can create APIs that are flexible, scalable, and easy to use, 
+making it possible to build complex web applications that can be consumed by a wide range of clients.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...
