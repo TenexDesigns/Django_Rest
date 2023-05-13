05b8e012662a48fc5f53dbd9aed506cd9c5641ec
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-13 15:24:59 +0300

    Create Shoppin_Api_Models.py

---
+++ Shoppin_Api_Models.py
@@ -0,0 +1,63 @@
+
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1)]   // You can put your  validators here
+    )
+
+    class Meta:
+        unique_together = [['cart', 'product']]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
