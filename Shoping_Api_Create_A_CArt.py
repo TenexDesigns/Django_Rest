0223239d47abe6cfa8ecd57e7ae94cbe10fd5a7b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-13 14:59:00 +0300

    Create Shoping_Api_Create_A_CArt.py

---
+++ Shoping_Api_Create_A_CArt.py
@@ -0,0 +1,94 @@
+SHOPING CART GET A CART, USE CART ID TO RETIVE THAT CARTS ITEMS AND EVEN DELET THAT CART, WE DONT ALLOW LIST , SO WE ONLY ALLOW POST
+
+
+
+1. SERIALIZER
+
+
+
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.quantity * cart_item.product.unit_price
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity', 'total_price']
+
+
+class CartSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'items', 'total_price']
+
+
+
+
+
+
+
+2. VIEWS 
+
+HERE WE DONT WANT TO LIST ALL OG OUR VIEWS, WE IMPLEMENT TH E GET POST AND DELETE METHODS
+
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  DestroyModelMixin,
+                  GenericViewSet):
+    queryset = Cart.objects.prefetch_related('items__product').all()
+    serializer_class = CartSerializer
+
+
+
+
+3. ROUTER
+
+router = routers.DefaultRouter()
+router.register('carts', views.CartViewSet)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
