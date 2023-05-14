ab1bf31f6e1976c1f22ff9f4e0c5eec54f5d27e8
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-14 20:18:00 +0300

    Update Customisng_User_Model.py

--- Customisng_User_Model.py
+++ Customisng_User_Model.py
@@ -189,85 +189,168 @@ class Customer(models.Model):
 
 
 
+NOW IN THE ADMIN FILE
 
 
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name',  'membership', 'orders']
+    list_editable = ['membership']
+    list_per_page = 10
+    list_select_related = ['user']      ------------------------------> We have to add this since we are refeing to another  model
+    ordering = ['user__first_name', 'user__last_name']             ---->We can t do that in the list_display as it is not suppoeted, we just created functions with the same names to be diplayed on the list display and reteuned the data to be dispayed on those functions
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
 
 
 
 
+MORE EXPLANATION
+**************************************************************************************************************************************************************
+Certainly! There are two common approaches to customizing the user model in Django: 
+ extending the existing User model and creating a separate profile model. Lets go through each approach:
 
 
+Approach 1: Extending the User Model
 
+1.Create a new Django project or open an existing one.
+2.In your Django project, locate the models.py file in your app directory.
+3.Import the AbstractUser class from django.contrib.auth.models:
 
+from django.contrib.auth.models import AbstractUser
 
+4.Create your custom user model by subclassing AbstractUser and adding any additional fields or methods you need:
 
+ class CustomUser(AbstractUser):
+    # Add custom fields and methods to your user model
+    pass
 
+5.In the settings.py file of your project, specify your custom user model by setting AUTH_USER_MODEL:
+ 
+AUTH_USER_MODEL = 'yourapp.CustomUser'
 
 
+Replace 'yourapp.CustomUser' with the app name and model name for your custom user model.
 
+6. Update any references to the User model in your project with the AUTH_USER_MODEL setting.
 
 
+7.Create and apply the necessary database migrations:
 
+python manage.py makemigrations
+python manage.py migrate
 
+Now your project will be using the extended custom user model.
 
 
 
 
 
+Approach 2: Creating a Profile Model
 
 
 
+1.Create a new Django project or open an existing one.
+2.In your Django project, locate the models.py file in your app directory.
+3.Import the User model from django.contrib.auth.models:
 
 
+from django.contrib.auth.models import User
 
 
+4.Create a separate profile model with a one-to-one relationship to the User model:
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Add additional fields for the profile
+   
+   Add any additional fields you need for the profile.
+   
+   
+5. In the settings.py file of your project, specify the AUTH_USER_MODEL setting to point to the User model:
 
+AUTH_USER_MODEL = 'auth.User'
 
 
+6.Update any references to the User model in your project to use the AUTH_USER_MODEL setting.
 
+7.Create and apply the necessary database migrations:
 
+ python manage.py makemigrations
+ python manage.py migrate
 
 
+With this approach, the User model remains unchanged, and you can access the profile model using a one-to-one relationship with the User model.
 
 
+Both approaches have their advantages and considerations.
+Extending the User model is suitable when you need to add fields directly to the user model and manage everything within a single table. 
+Creating a profile model is useful when you want to keep the User model separate and store additional information in a related profile model.
 
+Choose the approach that best fits your projects requirements and complexity.
 
 
 
 
 
+**************************************************************************************************************************************************************
 
 
+Customizing the user model in Django allows you to extend or modify the built-in User model according to your 
+applications requirements. Django provides a flexible authentication system that allows you to create a custom user model or modify the existing one.
 
+To customize the user model in Django, you'll need to perform the following steps:
 
 
 
+Step 1: Create a new Django project or open an existing one.
 
+Step 2: In your Django project, open the settings.py file and locate the AUTH_USER_MODEL setting.
+ If its not present, add it to the file. This setting specifies the custom user model for your project. For example:
 
+AUTH_USER_MODEL = 'myapp.CustomUser'
 
 
 
 
 
+Replace 'myapp.CustomUser' with the app name and model name for your custom user model.
 
+Step 3: Create a new model for your custom user by subclassing the AbstractUser or AbstractBaseUser class provided by Django.
+ If you want to extend the existing User model, use AbstractUser.
+ If you want to start from scratch, use AbstractBaseUser. Here's an example using AbstractUser:
 
 
+from django.contrib.auth.models import AbstractUser
 
+class CustomUser(AbstractUser):
+    # Add custom fields and methods to your user model
+    pass
 
 
 
 
+Add any additional fields or methods you need in your custom user model.
 
+Step 4: Update any references to the User model in your project with the AUTH_USER_MODEL setting. 
+ This includes foreign keys, many-to-many relationships, and any code that relies on the default User model.
 
+Step 5: Create and apply the database migrations to reflect the changes in your user model. Run the following commands:
 
 
+  python manage.py makemigrations
+python manage.py migrate
 
 
 
 
+These commands will create the necessary database tables or modify existing tables to accommodate your custom user model.
 
+After completing these steps, your Django project will be using the custom user model you defined. 
+You can use all the authentication and authorization features provided by Django with your custom user model.
+
+Remember that changing the user model after the project has started and data has been collected can be challenging.
+Its best to plan the user model customization before starting the project to avoid data migration issues.
 
 
 
