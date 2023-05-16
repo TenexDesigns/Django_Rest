9d959c66e51193d78bbdf15a99751c5802c48fd8
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-16 17:24:51 +0300

    Update Uploading_Media_Files.py

--- Uploading_Media_Files.py
+++ Uploading_Media_Files.py
@@ -46,6 +46,12 @@ if settings.DEBUG:
 
 
 
+4. Go to your models, where we want want to upload an image for a give product, so wecreate a many to one relationship between images and products because an product can have multiple images
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='images')  // This is used to establish the relationship between the products and the images
+    image = models.ImageField(upload_to='images',)  // Here we specify the pathh where the uploaded mages are to be stores i.e In our project we declared a folder named mmedia, then the uploades images are to be uploaded in media in a file caleed images
 
 
 
@@ -53,8 +59,27 @@ if settings.DEBUG:
 
 
 
+5. Go to your vies file 
+// The view.py
+class ProductImageViewSet(ModelViewSet):
+    serializer_class = ProductImageSerializer
 
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}             /// Here we send the productId to the serializer
 
+    def get_queryset(self):
+        return ProductImage.objects.filter(product_id=self.kwargs['product_pk'])
+
+// The serializer for the image
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return ProductImage.objects.create(product_id=product_id, **validated_data)  // Here we just set the id to the id sent from the url, the we just unpack the rest of the data
+
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image']
 
 
 
